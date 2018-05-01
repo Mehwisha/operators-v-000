7a1c964525d8e23c731154d38202c8677074f631
Mehwish Aziz <mehwisha@msn.com>
2018-05-01 01:16:14 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,10 @@
 def unsafe?(speed)
-if ((speed > 60) && (speed < 40)) ? true : false
+speed > 60 && speed < 40 ? true : false
 end
-end
+
 
 
 
 def not_safe?(speed)
-	if ((speed > 60) && (speed < 40)) ? true : false
-end
+	speed > 60 && speed < 40 ? true : false
 end
